0aace19811a73bccdd7c993273e5a711ec80a7db
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2021-05-04 00:03:23 +0200

    Problem 7B

--- UE7/Aufgabe7.py
+++ UE7/Aufgabe7.py
@@ -7,9 +7,19 @@ def insert_front(ordered_dict: OrderedDict, element: dict) -> None:
     ordered_dict.move_to_end(el_key, last=False)
 
 
+def get_common_elements(li1: list, li2: list) -> list:
+    return list(filter(lambda x: x in li2, li1))
+
+
 print('Problem A')
 t_dict = OrderedDict(a=12, b=152)
 el = {'c': 1}
 print('dict: ', dict(t_dict))
 insert_front(t_dict, el)
 print('inserted: ', dict(t_dict))
+
+print('\nProblem B')
+l1 = [1, 5, 5]
+l2 = [3, 4, 5, 5, 10]
+l3 = [5, 5, 10, 20]
+print(get_common_elements(get_common_elements(l1, l2), l3))
