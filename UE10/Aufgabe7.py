0886cec7ef0bc5ddbd31c3d10e165a8645458dcb
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2021-06-07 23:27:51 +0200

    Finished Task 7

---
+++ UE10/Aufgabe7.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x, y = np.meshgrid(np.arange(-1, 1, 0.1), np.arange(-2, 1, 0.1))
+x_val = np.sign(x)*(1-y)
+y_val = np.sign(x)*(1+x)
+plt.quiver(x, y, x_val, y_val, np.hypot(x_val, y_val))
+plt.title('Vector field')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
