346b1ee8cc34980ccd92a2f1a53caf726d071946
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2021-06-07 19:36:18 +0200

    Minor changes

---
+++ UE9/Aufgabe3.py
@@ -0,0 +1,4 @@
+import wrap
+
+for n in range(5):
+    print(wrap.series(n+1, 1.9, 2.0))
--- UE9/Aufgabe6.py
+++ UE9/Aufgabe6.py
@@ -17,30 +17,35 @@ import numpy as np
 #             return next_lamb, next_x
 #     return seq(A, tau, next_x, next_lamb)
 
-def poweriteration(A, tol, x):
+def power_iteration(A, tol, x):
     if not np.array_equal(A, A.T):
-        raise Exception("matrix has to be symmetric")
-    else:
-        l = 0
-        while True:
-            x = (A@x)/np.linalg.norm(A@x)
-            l_past = l
-            l = x@(A@x)
-            if np.linalg.norm(A @ x - l*x) <= tol:
-                if(np.abs(l) <= tol):
-                    if np.abs(l_past - l) <= tol:
-                        return (l, x)
-                elif np.abs(l_past - l) <= tol * np.abs(l):
-                    return (l, x)
+        raise Exception("A is not symmetric")
+
+    next_lam = 0
+    while True:
+        x = (A@x)/np.linalg.norm(A@x)
+        lam = next_lam
+        next_lam = x @ (A @ x)
+        if np.linalg.norm(A @ x - next_lam * x) <= tol:
+            if np.abs(next_lam) <= tol:
+                if np.abs(lam - next_lam) <= tol:
+                    return next_lam, x
+            if np.abs(lam - next_lam) <= tol * np.abs(next_lam):
+                return next_lam, x
 
 
 if __name__ == '__main__':
-    A = np.array([[3,4,5],
-                  [4,2,7],
-                  [5,7,9]])
-    x0 = np.array([2,3,4])
+    # error
+    # A = np.array([[1,2], [3,1]])
+    # x0 = np.array([2,3])
+    # print('poweriteration:\n', power_iteration(A, 10e-12, x0))
 
-    print('poweriteration:\n', poweriteration(A, 10e-12, x0))
+    A = np.array([[3,0,0],
+                  [0,1,0],
+                  [0,0,3]])
+    x0 = np.array([1,1,1])
+
+    print('poweriteration:\n', power_iteration(A, 10e-12, x0))
     print('\n\n', 'comparison:\n', np.linalg.eig(A)[0])
     print('\n\n', "corresponding eigenvectors:\n", np.linalg.eig(A)[1])
 
