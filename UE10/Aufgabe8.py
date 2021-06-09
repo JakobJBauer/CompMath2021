6c002c8e29a7792eca60678d06b5888c34e35dbd
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2021-06-09 18:10:50 +0200

    Improved Task 8

--- UE10/Aufgabe8.py
+++ UE10/Aufgabe8.py
@@ -1,47 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-circle = False
-ellipse = not circle
 
-l = np.arange(0, 2 * np.pi, np.pi/10)       # winkel
-k = np.arange(0, 1, 0.05)                   # abstand
-colors = np.array(["red", "green", "blue", "yellow", "black", "orange", "purple", "brown", "gray", "cyan", "magenta"])
+def draw_ellipse(y_scale=1.0):
+    l = np.arange(0, 2 * np.pi, np.pi / 10)
+    k = np.arange(0, 1, 0.05)
+    colors = np.array(['#1d79af', '#fd7f0d', '#2c9c2d', '#d1272b', '#9369be', '#8a5648', '#e279be', '#7f7e7e', '#bcbc24', '#1bbfcd'])
 
-fig = plt.figure()
-
-if circle:
+    fig = plt.figure()
     ax1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
-    j = 0
-    for i in k:
-        x = []
-        y = []
-        for r in l:
-            x.append(i * (np.cos(r)))
-            y.append(i * (np.sin(r)))
-        ax1.scatter(x, y, c=colors[j], s=9, marker="o")
-        j = (j+1) % len(colors)
+    for i in range(len(k)):
+        ax1.scatter(
+            np.array(list(map(lambda a: k[i] * np.cos(a), l))),
+            np.array(list(map(lambda a: k[i] * np.sin(a) * y_scale, l))),
+            c=colors[i % len(colors)], marker="o"
+        )
 
     ax1.legend(("circles",))
     ax1.set_xticks([-1, -0.5, 0, 0.5, 1])
     ax1.set_yticks([-1, -0.5, 0, 0.5, 1])
+    plt.show()
 
 
-if ellipse:
-    ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-
-    j = 0
-    for i in k:
-        x = []
-        y = []
-        for r in l:
-            x.append(i * (np.cos(r)))
-            y.append(i * (np.sin(r)) * 0.5)
-        ax2.scatter(x, y, c=colors[j], s=9, marker="o")
-        j = (j+1) % len(colors)
-
-    ax2.legend(("ellipses",))
-    ax2.set_xticks([-1, -0.5, 0, 0.5, 1])
-    ax2.set_yticks([-1, -0.5, 0, 0.5, 1])
-plt.show()
+if __name__ == '__main__':
+    draw_ellipse()            # circle
+    draw_ellipse(0.5)         # ellipse
