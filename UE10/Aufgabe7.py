e3c86ec5e4eab24a66d3446cdd5fe5f202be4f96
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2021-06-09 16:53:48 +0200

    Finished Task 7

--- UE10/Aufgabe7.py
+++ UE10/Aufgabe7.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x, y = np.meshgrid(np.arange(-1, 1, 0.1), np.arange(-2, 1, 0.1))
+x, y = np.meshgrid(np.arange(-1, 1, 0.1), np.arange(-2, 1, 0.1))    # koordinaten
 x_val = np.sign(x)*(1-y)
 y_val = np.sign(x)*(1+x)
 plt.quiver(x, y, x_val, y_val, np.hypot(x_val, y_val))
